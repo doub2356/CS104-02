f7b2e46aefc104879cd93876218b2d77b250c313
doub2356 <146123468+doub2356@users.noreply.github.com>
2023-10-04 20:33:32 -0400

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,22 @@
+# CS104
+# David Vozzo
+# conditions.py
+i = 0
+while i <= 5:
+    temp = int(input("Please enter the current temperature: "))
+    if temp > 90:
+        print("Wear Shorts")
+    elif temp > 70:
+        print("Short sleeves are fine")
+    elif temp > 50:
+        print("Wear a jacket")
+    elif temp > 32:
+        print("Wear a heavy coat")
+    else:
+        print("Stay Inside")
+    x = str(input("Would you like to enter another temperature: "))
+
+    if x == "no":
+        print("Enjoy the weather!")
+        break
+i += 1
